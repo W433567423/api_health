e212d747772e35d3dd94f8f90582671f33462756
tutu <t433567423@163.com>
2024-11-05 20:55:28 +0800

    feat: 更新用户实体

--- src/modules/user/entities/user.entity.ts
+++ src/modules/user/entities/user.entity.ts
@@ -24,41 +24,51 @@ export class UserEntity extends TimeEntity {
   @Column({ type: 'varchar', comment: '密码(加密后)', length: 20 })
   password: string;
 
-  @Column({ type: 'varchar', comment: '性别', length: 6 })
-  six: string;
+  @Column({ type: 'varchar', comment: '性别', length: 6, nullable: true })
+  six?: string;
 
-  @Column({ type: 'varchar', comment: '出生日期', length: 6 })
-  birthday: string;
+  @Column({ type: 'varchar', comment: '出生日期', length: 6, nullable: true })
+  birthday?: string;
 
-  @Column({ type: 'int', comment: '身高' })
-  height: number;
+  @Column({ type: 'int', comment: '身高', nullable: true })
+  height?: number;
 
-  @Column({ type: 'int', comment: '体重' })
-  weight: number;
+  @Column({ type: 'int', comment: '体重', nullable: true })
+  weight?: number;
 
-  @Column({ type: 'varchar', comment: '真实姓名' })
-  real_name: string;
+  @Column({ type: 'varchar', comment: '真实姓名', nullable: true })
+  real_name?: string;
 
-  @Column({ type: 'varchar', nullable: true, comment: '联系电话', length: 11 })
+  @Column({
+    type: 'varchar',
+    nullable: true,
+    comment: '联系电话',
+    length: 11,
+  })
   phone?: string;
 
-  @Column({ type: 'varchar', nullable: true, comment: '电子邮件', length: 20 })
+  @Column({
+    type: 'varchar',
+    nullable: true,
+    comment: '电子邮件',
+    length: 20,
+  })
   email?: string;
 
-  @Column({ type: 'varchar', comment: '省份', length: 10 })
-  province: string;
+  @Column({ type: 'varchar', comment: '省份', length: 10, nullable: true })
+  province?: string;
 
-  @Column({ type: 'varchar', comment: '城市', length: 10 })
-  city: string;
+  @Column({ type: 'varchar', comment: '城市', length: 10, nullable: true })
+  city?: string;
 
-  @Column({ type: 'varchar', comment: '详细地址', length: 40 })
-  address: string;
+  @Column({ type: 'varchar', comment: '详细地址', length: 40, nullable: true })
+  address?: string;
 
-  @Column({ type: 'varchar', comment: '身份证号', length: 18 })
-  card_id: string;
+  @Column({ type: 'varchar', comment: '身份证号', length: 18, nullable: true })
+  card_id?: string;
 
-  @Column({ type: 'varchar', comment: '民族', length: 10 })
-  nation: string;
+  @Column({ type: 'varchar', comment: '民族', length: 10, nullable: true })
+  nation?: string;
 
   @OneToOne(() => AvatarsEntity, (avatar) => avatar.user)
   avatar: AvatarsEntity;
